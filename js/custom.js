b57a84a49a76a79c5b0c3bc041142b2b9fd41987
Masum-Shakil <masumshakil@gmail.com>
2017-12-17 00:13:12 +0600

    practice

---
+++ js/custom.js
@@ -0,0 +1,92 @@
+//image light box
+$(document).ready(function() 
+{
+  $('.img_container').magnificPopup
+  ({
+ 	delegate: 'a', // child items selector, by clicking on it popup will open
+  	type: 'image',
+  	gallery:
+  	{
+    	enabled:true
+  	}
+ });
+});
+//logo
+$(document).ready(function() 
+{
+	$('#logo').owlCarousel({
+	    loop:true,
+	    smartSpeed:4000,
+	    margin:10,
+	    nav:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:5
+	        }
+	    }
+	})
+});
+//work carousel
+$(document).ready(function() 
+{
+	$('#owl_work').owlCarousel({
+	    loop:true,
+	    smartSpeed:4000,
+	    margin:10,
+	    nav:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:3
+	        }
+	    }
+	})
+});
+//quotes
+$(document).ready(function() 
+{
+	$('#customers').owlCarousel({
+	    loop:true,
+	    smartSpeed:4000,
+	    margin:10,
+	    nav:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:2
+	        }
+	    }
+	})
+});
+//navbar transition
+$(document).ready(function() 
+{
+	$(window).scroll(function()
+	{
+		var location=$(this).scrollTop();
+		if(location<70)
+		{
+			$("nav").removeClass("transparent");
+		}
+		else
+		{
+			$("nav").addClass("transparent");
+		}
+	});
+});
